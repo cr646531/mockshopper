7bc2e0e06b10bbb1ed9762a75de798fb0fde9549
Emily Farro <emily.farro@warbyparker.com>
2018-10-24 20:14:57 -0400

    index file with router

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+
+//ADD BODY PARSER
+
+//ADD STATIC FILES
+
+//ROUTING
+app.use('/api', require('./routes'));
+
+
+module.exports = app;
