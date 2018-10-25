2122454b6a3cffdbe6c33068ca58415b9c433345
Emily Farro <emily.farro@warbyparker.com>
2018-10-24 20:43:00 -0400

    WIP

---
+++ server/user.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+// load users
+router.get('/users', (req, res, next) => {
+  User.findAll()
+    .then(users => res.send(users))
+    .catch(next);
+});
+
+// edit user
+router.put('/users/:userId', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => user.update(req.body))
+    .then(user => res.send(user))
+    .catch(next);
+});
+
+// delete user
+router.delete('/users/:userId', (req, res, next) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => res.sendStatus(204))
+    .catch(next);
+});
+
+// create user
+router.post('/users/', (req, res, next) => {
+  User.create({ username: req.body.username })
+    .then(user => res.send(user))
+    .catch(next);
+});
