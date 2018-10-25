d751c997e1102c85493c5eb2193274d1cb57a5f2
Your Name <fernandez.Leovanny@gmail.com>
2018-10-24 20:09:49 -0400

    db file and Order model and route created

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgress://localhost:5432/my_db');
+// leaving the logging on in order to debug once we are ready to seed some data
+module.exports = {
+  db
+}
