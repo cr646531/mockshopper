1100c7421502893959c47f5db19a2480e2330b62
David Acconcio <dacconcio@gmail.com>
2018-10-27 18:11:43 -0400

    .eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
-{
+module.exports = {
   "extends": ["airbnb", "prettier"],
   "plugins": ["prettier"],
+	"parser": "babel-eslint",
+    "parserOptions": {
+        "sourceType": "module"
+    },
   "rules": {
     "prettier/prettier": ["error"]
   },
