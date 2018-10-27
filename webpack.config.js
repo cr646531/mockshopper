a6148e4c2147523a080c4213578d4b2a257cb335
David Acconcio <dacconcio@gmail.com>
2018-10-27 14:25:06 -0400

    fixed typo in webpack name

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ entry: ['babel-polyfill', './src/index.js'],
+ devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
