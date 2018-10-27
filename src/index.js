d757da7bacdb1ce7d64031e6ecb7964f790ee83e
David Acconcio <dacconcio@gmail.com>
2018-10-27 18:54:32 -0400

    added routes to react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { connect, Provider } from 'react-redux';
-import { Route, HashRouter } from 'react-router-dom';
-import Nav from './components/Nav'
-
+import { Switch, Route, HashRouter } from 'react-router-dom';
+import Nav from './components/Nav';
+import ProductPage from './components/ProductPage';
+import ProductDetailPage from './components/ProductDetailPage';
+import Cart from './components/Cart.js'
 
 class App extends Component {
   constructor() {
@@ -15,18 +17,23 @@ class App extends Component {
       <HashRouter>
         <div>
           <Route path="/" component={Nav} />
+          <Switch>
+            <Route exact path="/products" component={ProductPage} />
+            <Route path="/products/:productId" component={ProductDetailPage} />
+          </Switch>
+          <Route exact path='/cart' component={Cart} />
         </div>
       </HashRouter>
     );
   }
 }
 
-/*const ConnectedApp = connect(*/
-  //null,
-  //mapDispatchToProps
-//)(App);
-
 render(<App />, document.getElementById('root'));
+
+/*const ConnectedApp = connect(*/
+//null,
+//mapDispatchToProps
+//)(App);
 
 //render(
 //<Provider store={store}>
