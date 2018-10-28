76726aff820e5d02a843c45bae85a550bcebd852
Charlie <cr646531@gmail.com>
2018-10-28 18:20:11 -0400

    made changes to the names of some components

---
+++ src/components/Product.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  const categories = state.products.reduce((agg, product) => {
+    if (!agg.includes(product.category)) {
+      agg.push(product.category);
+      return agg;
+    } else {
+      return agg;
+    }
+  }, []);
+
+  return {
+    products: state.products,
+    categories
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+class Product extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.categories.map((category, index) => {
+          return (
+            <div key={index}>
+              {category}
+              <hr />
+              <br />
+              {this.props.products.map(product => {
+                if (product.category === category) {
+                  return (
+                    <div key={product.id}>
+                      <Link to={`/products/${product.id}`}>product.name</Link>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Product;
+
+//export default connect(
+//mapStateToProps,
+//mapDispatchToProps
+/*)(ProductPage);*/
---
+++ src/components/ProductDetail.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, props) => {
+  let student = {};
+
+  if (props.match.params.productId) {
+    product = state.products.find(product => {
+      return product.id === props.match.params.productId * 1;
+    });
+  } else {
+    console.log('No product');
+  }
+
+  return {
+    product
+  };
+};
+
+class ProductDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div> {this.props.product.name}</div>
+        <hr />
+        <br />
+        <div>{this.props.product.description}</div>
+      </div>
+    );
+  }
+}
+
+export default ProductDetail;
+
+//export default connect(
+//mapStateToProps,
+//mapDispatchToProps
+/*)(ProductDetailPage);*/
