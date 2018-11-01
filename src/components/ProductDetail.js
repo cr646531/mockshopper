41de97e28ace533bab77f7f79357432e1083189f
David Acconcio <dacconcio@gmail.com>
2018-10-31 20:09:08 -0400

    first commit

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state, props) => {
-  let student = {};
-
   if (props.match.params.productId) {
     product = state.products.find(product => {
       return product.id === props.match.params.productId * 1;
@@ -12,14 +10,39 @@ const mapStateToProps = (state, props) => {
     console.log('No product');
   }
 
+  const currentOrder = state.orders.find(order => order.status === 'CART');
+
   return {
-    product
+    products: state.products,
+    lineItems: state.lineItems,
+    currentOrder,
+    auth: state.auth
   };
 };
 
 class ProductDetail extends Component {
   constructor(props) {
     super(props);
+  }
+
+  addToCart() {
+    const productsLineItem = this.props.lineItems.find(lineItem => {
+      return (
+        lineItem.productId === productId &&
+        lineItem.orderId === this.props.currentOrder.id
+      );
+    });
+
+    if (!productsLineItem) {
+      this.props.createLineItem({
+        orderId: this.props.currentOrder.id,
+        quantity: 1,
+        productId
+      });
+    } else {
+      productsLineItem.quantity += 1;
+      this.props.updateLineItem(productsLineItem);
+    }
   }
 
   render() {
@@ -29,14 +52,12 @@ class ProductDetail extends Component {
         <hr />
         <br />
         <div>{this.props.product.description}</div>
+        <button onClick={this.addToCart}>Add to Cart</button>
       </div>
     );
   }
 }
 
-export default ProductDetail;
-
-//export default connect(
-//mapStateToProps,
-//mapDispatchToProps
-/*)(ProductDetailPage);*/
+export default connect(
+  mapStateToProps 
+)(ProductDetail);
